5c978bf584529cec72bec85620f7135d971a56dd
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-21 09:44:32 -0300

    Parameterized search query

--- main.py
+++ main.py
@@ -4,18 +4,32 @@ import send_email
 
 # API key from https://newsapi.org
 api_key = os.getenv("NEWS_API_KEY")
-url = f"https://newsapi.org/v2/everything?q=tesla&" \
-      f"from=2024-02-21&sortBy=publishedAt&apiKey={api_key}"
-# Content
+
+# Search parameters
+topic = "tesla"
+date = "2024-02-21"
+language = "en"
+url = f"https://newsapi.org/v2/everything?"\
+        f"q={topic}&" \
+        f"from={date}&"\
+        "sortBy=publishedAt&"\
+        f"apiKey={api_key}&"\
+        f"language={language}"
+
+# Get Content
 request = requests.get(url)
 content = request.json()
+
+# Create email message
 message = ""
+for articles in content["articles"][:20]:
+    if all([articles['title'], articles['description'], articles['url']]):
+        message = message + f"""Title: {articles['title']}
+Description: {articles['description']}
+See more at: {articles['url']}\n\n"""
 
-for articles in content["articles"][11:15]:
-    message = (message + f"""{articles['title']}
-{articles['url']}
-{articles['description']}\n\n""")
+message = "Subject: Email digest\n\n" + message
 
-message = "Subject: Email digest\n" + message
-
+# Send Email
+print(message)
 send_email.send_email(message.encode('utf-8'))
