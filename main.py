2952fa62fdd93d1724fa28f0ae9b1b2ceb2ec569
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-21 08:51:29 -0300

    Accessing Title, Description, and URL

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 import os
 
+# API key from https://newsapi.org
 api_key = os.getenv("NEWS_API_KEY")
 url = f"https://newsapi.org/v2/everything?q=tesla&" \
       f"from=2024-02-21&sortBy=publishedAt&apiKey={api_key}"
 
 request = requests.get(url)
-content = request.text
-print(content)
+
+content = request.json()
+
+for articles in content["articles"]:
+    print(articles["title"])
+    print(articles["description"])
+    print(articles["url"])
